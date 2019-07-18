a6a03d2c9427f1621428f3b784c4782cd0df148d
Han <yahyafarhan48@gmail.com>
2019-07-18 19:20:07 +0000

    cssmin

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,7 +13,18 @@ grunt.initConfig({
             dest: './sass/all.scss'
         },
         dev: {
-            src: ['./sass/*.scss'],
+            src: [
+                './sass/master.scss',
+                './sass/linear.scss',
+                './sass/layout.scss',
+                './sass/button.scss',
+                './sass/list.scss',
+                './sass/text.scss',
+                './sass/progress.scss',
+                './sass/input.scss',
+                './sass/header.scss',
+                './sass/identifiers.scss'
+            ],
             dest: './test/all.scss'
         }
     },
@@ -42,13 +53,24 @@ grunt.initConfig({
             }
         },
         dev: {
-            files: ['./sass/*.scss'],
+            files: ['./sass/*.scss','Gruntfile.js'],
             tasks: ['build-test'],
             options: {
                 spawn: false,
             }
         }
-    }
+    },
+    cssmin: {
+        options: {
+          mergeIntoShorthands: false,
+          roundingPrecision: -1
+        },
+        target: {
+          files: {
+            './dist/electro.min.css': ['./dist/electro.css']
+          }
+        }
+      }
 })
 
 //load plugins
@@ -56,9 +78,9 @@ grunt.loadNpmTasks('grunt-contrib-concat')
 grunt.loadNpmTasks('grunt-sass')
 grunt.loadNpmTasks('grunt-contrib-watch')
 grunt.loadNpmTasks('grunt-contrib-clean')
+grunt.loadNpmTasks('grunt-contrib-cssmin')
 
 //register events
-grunt.registerTask('build-all',['clean:build','concat:sass','sass:build'])
+grunt.registerTask('build-all',['clean:build','concat:sass','sass:build','cssmin'])
 grunt.registerTask('build-test', ['clean:dev','concat:dev','sass:dev'])
-
 }
